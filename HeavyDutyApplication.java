33a3b4d72b68a5ef7d0ec62138bac4950db5a3bf
BrunoOnateJim <bruno.onatejim@gmail.com>
2026-03-02 00:20:27 -0600

    Create HeavyDutyApplication.java

---
+++ HeavyDutyApplication.java
@@ -0,0 +1,11 @@
+package calorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavyDutyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HeavyDutyApplication.class, args);
+    }
+}
