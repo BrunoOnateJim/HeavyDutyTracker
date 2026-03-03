713efd8c9a2a8a2cfd9edb3c65f01acb384e923e
BrunoOnateJim <bruno.onatejim@gmail.com>
2026-03-03 11:20:54 -0600

    Update HeavyDutyApplication.java

--- HeavyDutyApplication.java
+++ HeavyDutyApplication.java
@@ -1,9 +1,9 @@
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"."})
 public class HeavyDutyApplication {
     public static void main(String[] args) {
         SpringApplication.run(HeavyDutyApplication.class, args);
