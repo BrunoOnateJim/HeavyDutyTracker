69889b87ba2411ea3ddc596d8a477ff7f33dbc04
BrunoOnateJim <bruno.onatejim@gmail.com>
2026-03-03 11:04:24 -0600

    Update Dias.java

--- Dias.java
+++ Dias.java
@@ -1,11 +1,14 @@
+import java.io.Serializable;
 
-public class Dias {
+public class Dias implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String fecha;
     private ListaEnlazada<Comidas> listaComidas;
     private double calT;
     private double proT;
     private double carbT;
     private double grasT;
+
     public Dias(String fecha, ListaEnlazada<Comidas> listacomidas, double calT, double proT, double carbT, double grasT){
         this.fecha = fecha;
         this.listaComidas = listacomidas;
@@ -14,11 +17,14 @@ public class Dias {
         this.carbT = carbT;
         this.grasT = grasT;
     }
+
     public String getFecha(){
         return fecha;
     }
+
     @Override
     public String toString(){
-        return "Fecha: "+fecha+"\n Calorias totales: "+calT+"\n Proteinas totales: "+proT+"\n Carbohidratos totales: "+carbT+"\n Grasa totales: "+grasT+"\n"+"Comidas en el dia:"+listaComidas.mostrarNodo();
+        return "Fecha: "+fecha+"\n Calorias totales: "+calT+"\n Proteinas totales: "+proT+"\n Carbohidratos totales: "+carbT+"\n Grasa totales: "+grasT;
     }
 }
+
